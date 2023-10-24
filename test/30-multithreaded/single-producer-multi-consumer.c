b8ea3158f3ed4a2381a4add708eef4cbad8ddffb
Nat! <nat@mulle-kybernetik.com>
2023-10-24 17:32:03 +0200

    cherry-pick fixed tests from master

--- test/30-multithreaded/single-producer-multi-consumer.c
+++ test/30-multithreaded/single-producer-multi-consumer.c
@@ -4,15 +4,28 @@
 #include <stdlib.h>
 
 
+#if 1
+# define N_CONSUMERS     3
+# define N_ITEMS         1024
+# define N_QUEUE_ITEMS   100
+#else
+# define N_CONSUMERS     3
+# define N_ITEMS         64
+# define N_QUEUE_ITEMS   4
+#endif
+
+
 static mulle_thread_rval_t   producer( void *arg)
 {
    struct mulle_pointermultififo  *fifo = arg;
    struct timespec                 delay;
    int                             i;
+   int                             warned;
 
-   fprintf( stderr, "producer starts\n");
+   fprintf( stderr, "producer 0x%tx starts\n", (intptr_t) mulle_thread_self());
 
-   i = 1;
+   warned = 0;
+   i      = 1;
    for(;;)
    {
       switch( rand() % 8)
@@ -23,11 +36,21 @@ static mulle_thread_rval_t   producer( void *arg)
       case 3 :
          if( _mulle_pointermultififo_write( fifo, (void *) (intptr_t) i) == -1)
          {
-            fprintf( stderr, "producer full\n");
+            if( ! warned)
+            {
+               fprintf( stderr, "producer 0x%tx full\n", (intptr_t) mulle_thread_self());
+               warned = 1;
+            }
          }
          else
          {
-            if( i == 1024)
+            if( warned)
+            {
+               fprintf( stderr, "producer 0x%tx continues\n", (intptr_t) mulle_thread_self());
+               warned = 0;
+            }
+
+            if( i == N_ITEMS)
                goto done;
             ++i;
          }
@@ -44,7 +67,7 @@ static mulle_thread_rval_t   producer( void *arg)
    }
 
 done:
-   fprintf( stderr, "producer quits\n");
+   fprintf( stderr, "producer 0x%tx quits (i=%d)\n", (intptr_t) mulle_thread_self(), i);
    mulle_thread_return();
 }
 
@@ -55,8 +78,13 @@ static mulle_thread_rval_t   consumer( void *arg)
    struct timespec                 delay;
    void                            *pointer;
    int                             i;
+   int                             warned;
+   unsigned int                    count;
 
-   i = 3;
+   fprintf( stderr, "consumer 0x%tx starts\n", (intptr_t) mulle_thread_self());
+
+   count  = 0;
+   warned = 0;
    for(;;)
    {
       switch( rand() % 8)
@@ -68,15 +96,23 @@ static mulle_thread_rval_t   consumer( void *arg)
          pointer = _mulle_pointermultififo_read_barrier( fifo);
          if( ! pointer)
          {
-            fprintf( stderr, "consumer empty\n");
+            if( ! warned)
+            {
+               fprintf( stderr, "consumer 0x%tx empty\n", (intptr_t) mulle_thread_self());
+               warned = 1;
+            }
             continue;
          }
-         printf( "%ld\n", (long) (intptr_t) pointer); fflush( stdout);
-         if( pointer == (void *) 1024)
+
+         if( warned)
          {
-            if( ! --i)
-               goto done;
+            fprintf( stderr, "consumer 0x%tx continues\n", (intptr_t) mulle_thread_self());
+            warned = 0;
          }
+
+         printf( "%ld\n", (long) (intptr_t) pointer); fflush( stdout);
+         if( (intptr_t) pointer >= N_ITEMS - (N_CONSUMERS - 1))
+            goto done;
          break;
 
       case 4 :
@@ -90,7 +126,7 @@ static mulle_thread_rval_t   consumer( void *arg)
    }
 
 done:
-   fprintf( stderr, "consumer quits\n");
+   fprintf( stderr, "consumer 0x%tx quits\n", (intptr_t) mulle_thread_self());
    mulle_thread_return();
 }
 
@@ -99,21 +135,20 @@ done:
 int  main( int argc, char *argv[])
 {
    struct mulle_pointermultififo  fifo;
-   mulle_thread_t                 producer_thread[ 3];
-   mulle_thread_t                 consumer_thread[ 1];
+   mulle_thread_t                 consumer_thread[ N_CONSUMERS];
    int                            i;
 
-   _mulle_pointermultififo_init( &fifo, 100, NULL);
+   _mulle_pointermultififo_init( &fifo, N_QUEUE_ITEMS, NULL);
 
-   for( i = 0; i < 3; i++)
-      mulle_thread_create( producer, &fifo, &producer_thread[ i]);
+   for( i = 0; i < N_CONSUMERS; i++)
+      mulle_thread_create( consumer, &fifo, &consumer_thread[ i]);
 
-   fprintf( stderr, "READ:\n");
-   mulle_thread_create( consumer, &fifo, &consumer_thread[ 0]);
+   producer( &fifo);
+//   mulle_thread_create( producer, &fifo, &producer_thread[ 0]);
 
-   for( i = 0; i < 3; i++)
-      mulle_thread_join( producer_thread[ i]);
-   mulle_thread_join( consumer_thread[ 0]);
+   for( i = 0; i < N_CONSUMERS; i++)
+      mulle_thread_join( consumer_thread[ i]);
+//   mulle_thread_join( producer_thread[ 0]);
 
    _mulle_pointermultififo_done( &fifo);
 
